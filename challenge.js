1e529288cbe841b7fef2ba73e6508b670cb3efcf
MaryannAY <maryann.ahmed2@gmail.com>
2023-07-28 13:22:28 -0700

    solved rock-paper-scissors

--- challenge.js
+++ challenge.js
@@ -1,2 +1,16 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function playgame(player1, player2){
+if (player1==player2) 
+   return "Tie"
+} 
+ if (player1 == "rock" && player2 == "scissors"|| 
+player1 == "paper" && player2 == "rock") 
+{
+   return "player1 wins"
+} 
+else {
+   return "player2 wins"
+}
+
+console.log (playgame("rock","paper"))
